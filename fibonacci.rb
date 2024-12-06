86237d4a521172da75d3a0f028e2de245c5b77b1
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-12-06 01:30:57 -0500

    Add fibonacci calculation method using recursion

--- fibonacci.rb
+++ fibonacci.rb
@@ -10,8 +10,18 @@ def fibs(n)
   fib_array
 end
 
+def fibs_rec(n)
+  
+  return [] if n < 1
+  return [0] if n == 1
+  return [0, 1] if n == 2
+    
+  fib_array = fibs_rec(n-1)
+  fib_array << (fib_array[-1] + fib_array[-2])
+  return fib_array
+end
+
 puts "Welcome to fibonacci sequence calculator"
 puts "How many number of the sequence do you want to see?"
 n = gets.to_i
-
-p fibs(n)
+p fibs_rec(n)
