6a474e0d825ad4173a3b51e20c3bcfe4686118f7
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-12-08 00:05:47 -0500

    stack level too deep fixed
    the error was on line 6 array[0..mid] instead of array[0...mid]

--- merge_sort.rb
+++ merge_sort.rb
@@ -3,14 +3,16 @@ require "rubocop"
 def merge_sort(array)
   sorted_array = []
   mid = array.length / 2
-  left = array[0..mid]
+  left = array[0...mid]
   right = array[mid..-1]
   return array if array.length <= 1
 
   left_sorted = merge_sort(left)
   right_sorted = merge_sort(right)
-  i, j = 0
-  while i < left_sorted && j < right_sorted
+  i = 0
+  j = 0
+
+  while i < left_sorted.length && j < right_sorted.length
     if left_sorted[i] < right_sorted[j]
       sorted_array << left_sorted[i]
       i += 1
@@ -18,13 +20,14 @@ def merge_sort(array)
       sorted_array << right_sorted[j]
       j += 1
     end
-    if i >= left_sorted.length
-      sorted_array << right_sorted
-    elsif j >= right_sorted.length
-      sorted_array << left_sorted
+
+    if i >= left_sorted.length && j < right_sorted.length
+      sorted_array.concat(right_sorted[j..-1])
+    elsif j >= right_sorted.length && i < left_sorted.length
+      sorted_array.concat(left_sorted[i..-1])
     end
   end
   sorted_array
 end
 
-p merge_sort([3, 2, 1, 13, 8, 5, 0, 1])
+p merge_sort([105, 79, 100, 110])
